7e0e13ade942715318777c99fbd48462f811710c
Jerzy Ziulkowski <jerry.111@o2.pl>
2023-03-08 22:32:07 +0100

    Add new date

--- wizytowki.py
+++ wizytowki.py
@@ -8,5 +8,12 @@ class VCard:
         self.nazwa_firmy = nazwa_firmy
         self.stanowisko = stanowisko
         self.e_mail = e_mail
-nowak = VCard(imie="Jan", nazwisko="Nowak", nazwa_firmy="Luxmed", stanowisko="manager", e_mail = "jnowak@luxmed.pl")
-print(nowak)
+jnowak = VCard(imie="Jan", nazwisko="Nowak", nazwa_firmy="Luxmed", stanowisko="manager", e_mail = "jnowak@luxmed.pl")
+print(jnowak.stanowisko)
+akowalski = VCard(imie="Andrzej", nazwisko="Kowalski", nazwa_firmy="Commarch", stanowisko="senior specialist", e_mail="akowalski@commarch.com")
+print(akowalski.stanowisko)
+aklin = VCard(imie="Andrzej", nazwisko="Klin", nazwa_firmy="Merlin", stanowisko="junior specialist", e_mail = "aklin@merlin.pl")
+print(aklin.stanowisko)
+mstelmaszczyk = VCard(imie="Mirosław", nazwisko="Stelmaszczyk", nazwa_firmy="Stekop", stanowisko="manager", e_mail = "mstelmaszczyk@stekop.pl")
+print(mstelmaszczyk.stanowisko)
+bniewiadomski = VCard(imie="Bogdan", nazwisko="Niewiadomski", nazwa_firmy="Solid Security", stanowisko="specialist", e_mail = "bniewiadomski@solid.eu")
